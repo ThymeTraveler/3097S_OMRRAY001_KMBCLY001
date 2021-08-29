10cf72b0408a3014471d4d6867a2dc99f4a01f45
Rayhaan Omar <rayhaan2120@gmail.com>
2021-08-29 18:16:16 +0200

    .wav compression working

--- Python/toMp3.py
+++ Python/toMp3.py
@@ -1,9 +1,16 @@
-import numpy
+import numpy as np
+import wave
 
 fileName = "input.txt"
 f = open(fileName, "r") #open file for reading
 ArrayofString = f.read().splitlines() #enter file lines into array
 
+ArrayOfFloat = np.asarray(ArrayofString, dtype=np.float64, order='C')
 
 import soundfile as sf
-sf.write('sound.wav', yourArray, 48000)
+sf.write('sound.wav', ArrayOfFloat, 48000)
+
+data, sampleRate = sf.read('sound.wav')
+
+for I in data:
+        print(I)
