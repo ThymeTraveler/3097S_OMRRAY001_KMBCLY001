4c61c739aa1d8d2a34af595bd47d60dfcf30db73
Rayhaan Omar <rayhaan2120@gmail.com>
2021-08-29 21:15:02 +0200

    mp3 compression now working

--- Python/toMp3.py
+++ Python/toMp3.py
@@ -13,6 +13,13 @@ sf.write('sound.wav', ArrayOfFloat, 48000)
 #data, sampleRate = sf.read('sound.wav')
 
 wav = 'sound.wav'
-cmd = 'lame --preset insane %s' % wav
+cmd = 'lame --preset standard %s' % wav
 import subprocess
 subprocess.call(cmd, shell=True)
+
+#the commented out block below would be used for extracting the information from the mp3
+#import audio2numpy as a2n
+#x,sr=a2n.audio_from_file("sound.mp3")
+
+#for i in x:
+ #   print(i)
